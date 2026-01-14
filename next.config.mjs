e8697b9d2d32bf23ee6d3ce1478a993185cbc91f
Олег <oleg.zhuravlev.2020@mail.ru>
2026-01-14 13:09:43 +0300

    fix next config for vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { images: { unoptimized: true } };
+const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+};
+
 export default nextConfig;
